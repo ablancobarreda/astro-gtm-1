f6a49755905958f0feb2ecf894469cc8014c2f98
Antonio Alejandro <aablancobarreda@gmail.com>
2024-10-04 10:27:39 -0400

    come on

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
       config: {
         forward: ["dataLayer.push"],
         resolveUrl: (url) => {
-          const proxyUrl = new URL('https://astro-gtm.netlify.app/proxy');
+          const proxyUrl = new URL('https://coruscating-peony-1ea3c6.netlify.app/proxy');
           proxyUrl.searchParams.append('url', url.href);
           return proxyUrl;
         },
